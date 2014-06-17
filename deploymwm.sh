62719cfeac45d832bb718622acf00cc03144f62b
Adam Horner <adam@horner.org.uk>
2014-06-17 09:21:34 +0100

    deployment management

---
+++ deploymwm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this script depends on a script in Adam Horner's environment that is not in
+# the public domain (sorry).
+
+deploy.sh \
+    -o macWindowScript.bash \
+    -o moveWindow.scpt \
+    -o printWindowXYWH.scpt \
+    -o hmscreens \
+    -d ~/Dropbox/Synchronisation/Alfred/Alfred.alfredpreferences/workflows/user.workflow.93D9B6E3-96ED-457C-969C-1C47AAD7C297
